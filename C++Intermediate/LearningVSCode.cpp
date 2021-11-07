ad488459afb73c9bf9daa0c1bee771c0f2bcf53f
Gerard E. Paul <gerardpaul@outlook.com>
2021-11-07 11:25:54 -0600

    changes made. Print function working.

--- C++Intermediate/LearningVSCode.cpp
+++ C++Intermediate/LearningVSCode.cpp
@@ -5,23 +5,10 @@
 
 using namespace std;
 
-
-char PrintString(char input[])
-{
-    char response;
-    for (int i{0}; i != '\0'; i++)
-    {
-        response = input[i];
-        cout << response << endl;
-
-       
-    }
-     return response++;
-}
-
 int GetLength(char input[]) // make sure function takes in the same value as intended.
 {
     int count{0};
+    
 
     for ( int c = {0}; input[c] != '\0'; c++)
     {count = input[c];}
@@ -35,30 +22,43 @@ void ReverseString(char input[])
     swap(start, end);
 }
 
+void Print(char input[])
+{
+   
+
+    for(int i{0};  input[i] != '\0'; ++i)
+    {
+     
+      printf("%c",input[i]); 
+    }
+
+       
+
+
+
+}
 int main()
 {
-    int limit{6};
+    int limit{2};
     char input[100];
     char response;
 
     // should be space for the null charcter.
     cout << "Enter you name: " << endl;
 
-    for( int j{0}; j < sizeof(input); j++)
+    for( int j{0}; j < limit; j++)
     {
         cin.getline(input, 100);
 
-        if (sizeof(response) < input[j])
-        {
-
-            response = input[j];
-            printf("%c", response);
-            PrintString(input); 
-        }
+       
     
+            
+          Print(input); 
+        
+
     }
 
     system("pause");
 
-    ;
+    
 }
