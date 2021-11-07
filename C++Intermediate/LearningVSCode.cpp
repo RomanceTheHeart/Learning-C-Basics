43615a5ce306c895b2c011b01b46301d1c9af268
Gerard E. Paul <gerardpaul@outlook.com>
2021-11-07 09:12:49 -0600

    Added functions to processes chars

--- C++Intermediate/LearningVSCode.cpp
+++ C++Intermediate/LearningVSCode.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 #include <stdio.h>
+#include "string"
 #include <array>
 
 using namespace std;
+
+
+char PrintString(char input[])
+{
+    char response;
+    for (int i{0}; i != '\0'; i++)
+    {
+        response = input[i];
+        cout << response << endl;
+
+       
+    }
+     return response++;
+}
+
+int GetLength(char input[]) // make sure function takes in the same value as intended.
+{
+    int count{0};
+
+    for ( int c = {0}; input[c] != '\0'; c++)
+    {count = input[c];}
+        return count++;
+}
+
+void ReverseString(char input[])
+{
+    int start = 0;
+    int end = {GetLength(input)};
+    swap(start, end);
+}
 
 int main()
 {
@@ -13,7 +44,7 @@ int main()
     // should be space for the null charcter.
     cout << "Enter you name: " << endl;
 
-    for (int j{0}; j < sizeof(input); j++)
+    for( int j{0}; j < sizeof(input); j++)
     {
         cin.getline(input, 100);
 
@@ -22,12 +53,12 @@ int main()
 
             response = input[j];
             printf("%c", response);
+            PrintString(input); 
         }
-        else{break;}
+    
     }
-
 
     system("pause");
 
-    return 0;
+    ;
 }
