8c5ec1323b19799b47b08248ba5ff724c167f3fb
Gerard E. Paul <gerardpaul@outlook.com>
2021-11-11 21:56:03 -0600

    Finally got it to work

--- C++Intermediate/LearningVSCode.cpp
+++ C++Intermediate/LearningVSCode.cpp
@@ -8,81 +8,80 @@ using namespace std;
 
 int GetLength(char input[]) // make sure function takes in the same value as intended.
 {
-    int count{0};
-    
+  int count{0};
 
-    for ( int c = {0}; input[c] != '\0'; c++)
-    {
-     count++; 
-        
-    }
-  
-           return count;
-}
-
-char ReverseString(char input[])
-{
-    int start = input[0];
-    int end = {GetLength(input)};
-    char result; 
-
-  while(start <= end)
+  for (int c = {0}; input[c] != '\0'; c++)
   {
-     start++; 
-     end--; 
-        result = input[start,end]; 
-        cout << result; 
-        cout << "\nFuck this!!" << endl; 
-
+    count++;
   }
 
-  return result; 
+  return count;
+}
+
+void ReverseString(char input[])
+{
+  int start = input[0];
+  int end = {GetLength(input)};
+  char result;
+
+  while (start > end )
+  {
+
+    swap(input[start], input[end]);
+
+
+
+
+    start++;
+    end--;
+        if (start == '\0' || end == '\0')
+    {
+      break;
+    }
+     
+
+  cout << input[start,end];
+
+  }
+  
+
 }
 
 void Print(char input[])
 {
-   
 
-    for(int i{0};  input[i] != '\0'; ++i)
-    {
-     
-      printf("%c",input[i]); 
-    }
+  for (int i{0}; input[i] != '\0'; ++i)
+  {
 
-       
-
-
-
+    printf("%c", input[i]);
+  }
 }
 int main()
 {
 
+  int limit{1};
+  int count;
 
-    int limit{1};
-    int count; 
+  char input[100];
 
-    char input[100];
+  char response;
 
-    char response;
+  cout << "Enter you name: " << endl;
 
-    cout << "Enter you name: " << endl;
+  for (int j{0}; j != limit; j++)
+  {
+    cin.getline(input, 100);
 
-    for( int j{0}; j != limit; j++)
-    {
-        cin.getline(input, 100);
+    ReverseString(input);
 
-       
-        ReverseString(input); 
-            
-          Print(input); 
+    Print(input);
 
-          cout << "\nThe length of your string is: "; 
-          count = GetLength(input); 
-          cout << count << "\n"; 
-        
-    }
+    cout << "\nThe length of your string is: ";
 
-    system("pause");
+    count = GetLength(input);
 
-    
+    cout << count << "\n";
+  }
+
+  system("pause");
 }
